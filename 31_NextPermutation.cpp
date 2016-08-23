467a5473135cc0e6d207fcbcb79b4e5088bba455
wangxiaobao1114 <942939879@qq.com>
2016-08-23 21:49:20 +0800

    Next Permutation

---
+++ 31_NextPermutation.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        if (nums.size() < 2) {
+            return;
+        }
+        int i = nums.size() - 2;
+        for (i; i >= 0; i--) {
+            if (nums[i] < nums[i+1]) {
+                break;
+            }
+        }
+        if (i == -1) {
+            sort(nums.begin(), nums.end());
+            return;
+        }
+        int j = nums.size() - 1;
+        for (j; j > i; j--) {
+            if (nums[j] > nums[i]) {
+                break;
+            }
+        }
+        swap(nums[i], nums[j]);
+        reverse(nums.begin() + i + 1, nums.end());
+        
+    }
+};
